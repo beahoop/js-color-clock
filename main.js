31adba01bb07fa39f297f5e48e3d178af668ec6a
beahoop <beahoopdesign@gmail.com>
2021-01-13 15:59:00 -0500

    progress bar updated

--- main.js
+++ main.js
@@ -5,28 +5,33 @@
 // then i want to set it to a Variables
 // the var should replace 00:00:00 in the html
 
-var date = new Date(); /* creating object of Date class */
-var hour = date.getHours();
-var min = date.getMinutes();
-var sec = date.getSeconds();
-hour = updateTime(hour);
-min = updateTime(min);
-sec = updateTime(sec);
-var t = setTimeout(function(){ currentTime() }, 1000);
   function currentTime() {
-  // var date = new Date(); /* creating object of Date class */
-  // var hour = date.getHours();
-  // var min = date.getMinutes();
-  // var sec = date.getSeconds();
-  // hour = updateTime(hour);
-  // min = updateTime(min);
-  // sec = updateTime(sec);
+  var date = new Date(); /* creating object of Date class */
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
   document.querySelector('.clock-display').innerHTML = hour + " : " + min + " : " + sec;
   //adding time to the div
-    // var t = setTimeout(function(){ currentTime() }, 1000);
+    var t = setTimeout(function(){ currentTime() }, 1000);
     //setting timer
-}
+    function percentage(num){
+      return (num/60).toString() + "em";
+    }
 
+    var progressBarFull = document.querySelector('.clock-progress-bar');
+   progressBarFull.style.width = percentage(sec);
+   //needs to return the sec percent as a data type css can read
+
+  // Using the percent above, dynamically modify the length of the timer bar
+  // // document.querySelector('.clock-progress-bar').innerHTML =
+    console.log(percentage(sec));
+    // document.getElementsByClassName('clock-progress-bar').innerHTML = percentage(sec);
+
+
+}
 function updateTime(x) {
   if (x < 10) {
     return "0" + x;
@@ -41,10 +46,6 @@ currentTime(); /* calling currentTime() function to initiate the process */
 
 //console.log the percentage of a minute that the current seconds represents (e.g., if 30 seconds have elapsed, console.log 0.5)
 
-function percentage(num){
-  return (num/60);
-}
-console.log(percentage(sec));
 
 
 
